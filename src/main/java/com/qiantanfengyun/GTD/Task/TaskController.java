330ec017fc5912c48cba37b7958c97eb0561bde4
rongjieaaa <rongjieaaa@gmail.com>
2020-09-13 22:43:24 +0800

    完善：Post的API改为使用@RequestBody注解、移除不必要的import语句、修正方法名。

--- src/main/java/com/qiantanfengyun/GTD/Task/TaskController.java
+++ src/main/java/com/qiantanfengyun/GTD/Task/TaskController.java
@@ -1,23 +1,22 @@
 package com.qiantanfengyun.GTD.Task;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/task")
 public class TaskController {
 
     @Autowired
     private TaskRepository taskRepository;
 
-    @PostMapping(path="/add")
-    public @ResponseBody String addTask (@RequestParam Task task) {
+    @PostMapping(path="/save")
+    public @ResponseBody String save (@RequestBody Task task) {
         taskRepository.save(task);
         return "Saved";
     }
     @GetMapping(path="/all")
-    public @ResponseBody Iterable<Task> getAllTasks () {
+    public @ResponseBody Iterable<Task> all () {
         return taskRepository.findAll();
     }
 
